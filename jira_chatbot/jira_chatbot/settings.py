cc19b5cf284d3810a9dacd36cb4cd6138ba04a91
Kevin <kevinolm10@gmail.com>
2025-08-11 21:41:30 +0200

    Set up allowed hosts for accessing the dev server

--- jira_chatbot/jira_chatbot/settings.py
+++ jira_chatbot/jira_chatbot/settings.py
@@ -12,7 +12,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = [
-    
+    '127.0.0.1',
+    'localhost',
+    'localhost:8000'
 ]
 
 
@@ -42,7 +44,7 @@ ROOT_URLCONF = 'jira_chatbot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
