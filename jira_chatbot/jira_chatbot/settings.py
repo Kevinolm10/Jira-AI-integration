4cf0575b03d2ac4810550b7a49ceef556be8f864
Kevin <kevinolm10@gmail.com>
2025-08-22 13:04:20 +0200

    Add csrf token for the chatbot form

--- jira_chatbot/jira_chatbot/settings.py
+++ jira_chatbot/jira_chatbot/settings.py
@@ -107,6 +107,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Additional locations of static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'jira_chatbot' / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
