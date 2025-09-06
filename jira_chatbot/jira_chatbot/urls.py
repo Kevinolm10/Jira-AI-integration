243cf3bbfc3aefb1894cd5dd2ae68f37c4ca92d3
kevin ölmheden <kevinolm10@gmail.com>
2025-09-06 22:44:55 +0200

    Add URL routing for the auth system

--- jira_chatbot/jira_chatbot/urls.py
+++ jira_chatbot/jira_chatbot/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('jiraAuth.urls')),
     path('', include('ai_chat.urls'))
 ]
 
